827a019a7bfb13fdc8499b4898d8503a18e155cf
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-19 23:23:48 -0400

    creatin FormArray FormControl for reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+  ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
